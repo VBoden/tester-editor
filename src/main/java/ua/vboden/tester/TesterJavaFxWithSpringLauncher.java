febf1807e7ae9dabbfa536013500d1ac7dd631db
Володимир Боденчук <Bodenchuk@bigmir.net>
2024-02-10 16:53:50 +0200

    Select db on start.

--- src/main/java/ua/vboden/tester/TesterJavaFxWithSpringLauncher.java
+++ src/main/java/ua/vboden/tester/TesterJavaFxWithSpringLauncher.java
@@ -19,6 +19,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import ua.vboden.tester.controllers.AbstractController;
+import ua.vboden.tester.services.PreferencesService;
 
 @Configuration
 @SpringBootApplication
@@ -50,49 +51,49 @@ public class TesterJavaFxWithSpringLauncher extends Application {
 	}
 
 	private String[] selectDbFile() {
-//		String lastDB = PreferencesService.getLastDB();
-//		if (!PreferencesService.isSelectDbOnStart() && lastDB != null && !(new File(lastDB).exists())) {
-//			infoBox(String.format(resources.getString("start.last.db.not.found").replaceAll("\\n", "\n"), lastDB),
-//					resources.getString("start.file.not.found"));
-//		}
-//		if (PreferencesService.isSelectDbOnStart() || lastDB == null || !(new File(lastDB).exists())) {
-//			JFileChooser fileChooser = new JFileChooser();
-//			FileFilter filter = new FileNameExtensionFilter(resources.getString("start.file.filter.name"), "sqlite3");
-//			fileChooser.setFileFilter(filter);
-//			fileChooser.setDialogTitle(resources.getString("start.file.select.title"));
-//			if (lastDB != null && new File(lastDB).exists()) {
-//				fileChooser.setCurrentDirectory(new File(lastDB));
-//			} else {
-//				fileChooser.setCurrentDirectory(new File("."));
-//			}
-//			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-//			fileChooser.showOpenDialog(null);
-//			File file = fileChooser.getSelectedFile();
-//			if (file != null) {
-//				dbPath = file.getAbsolutePath();
-//				PreferencesService.setLastDB(dbPath);
-//				return new String[] { String.format("--%s=%s", URL_KEY, DB_PREFIX + file.getAbsolutePath()) };
-//			} else {
-//
-//				if (dbPath == null) {
-//					String defaultDB = (new File(DOT).getAbsolutePath() + appProperties.getString(URL_KEY))
-//							.replace(DOT + DB_PREFIX, "");
-//					if (!(new File(defaultDB).exists())) {
-//						infoBox(String.format(resources.getString("start.file.not.selected.no.default").replaceAll("\\n", "\n"), defaultDB),
-//								resources.getString("start.file.not.selected.no.default.title"));
-//						Platform.exit();
-//						System.exit(0);
-//					} else {
-//						infoBox(String.format(resources.getString("start.file.not.selected").replaceAll("\\n", "\n"), defaultDB),
-//								resources.getString("start.file.not.selected.title"));
-//					}
-//					dbPath = defaultDB;
-//				}
-//			}
-//		} else if (lastDB != null && new File(lastDB).exists()) {
-//			dbPath = lastDB;
-//			return new String[] { String.format("--%s=%s", URL_KEY, DB_PREFIX + lastDB) };
-//		}
+		String lastDB = PreferencesService.getLastDB();
+		if (!PreferencesService.isSelectDbOnStart() && lastDB != null && !(new File(lastDB).exists())) {
+			infoBox(String.format(resources.getString("start.last.db.not.found").replaceAll("\\n", "\n"), lastDB),
+					resources.getString("start.file.not.found"));
+		}
+		if (PreferencesService.isSelectDbOnStart() || lastDB == null || !(new File(lastDB).exists())) {
+			JFileChooser fileChooser = new JFileChooser();
+			FileFilter filter = new FileNameExtensionFilter(resources.getString("start.file.filter.name"), "sqlite3");
+			fileChooser.setFileFilter(filter);
+			fileChooser.setDialogTitle(resources.getString("start.file.select.title"));
+			if (lastDB != null && new File(lastDB).exists()) {
+				fileChooser.setCurrentDirectory(new File(lastDB));
+			} else {
+				fileChooser.setCurrentDirectory(new File("."));
+			}
+			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			fileChooser.showOpenDialog(null);
+			File file = fileChooser.getSelectedFile();
+			if (file != null) {
+				dbPath = file.getAbsolutePath();
+				PreferencesService.setLastDB(dbPath);
+				return new String[] { String.format("--%s=%s", URL_KEY, DB_PREFIX + file.getAbsolutePath()) };
+			} else {
+
+				if (dbPath == null) {
+					String defaultDB = (new File(DOT).getAbsolutePath() + appProperties.getString(URL_KEY))
+							.replace(DOT + DB_PREFIX, "");
+					if (!(new File(defaultDB).exists())) {
+						infoBox(String.format(resources.getString("start.file.not.selected.no.default").replaceAll("\\n", "\n"), defaultDB),
+								resources.getString("start.file.not.selected.no.default.title"));
+						Platform.exit();
+						System.exit(0);
+					} else {
+						infoBox(String.format(resources.getString("start.file.not.selected").replaceAll("\\n", "\n"), defaultDB),
+								resources.getString("start.file.not.selected.title"));
+					}
+					dbPath = defaultDB;
+				}
+			}
+		} else if (lastDB != null && new File(lastDB).exists()) {
+			dbPath = lastDB;
+			return new String[] { String.format("--%s=%s", URL_KEY, DB_PREFIX + lastDB) };
+		}
 		return getParameters().getRaw().toArray(new String[0]);
 	}
 
